1c8e88d6df4f725650547f4f5f229b0f610b314c
Chew <chew@chew.pw>
2022-01-13 12:56:29 -0600

    Copyright headers and more docs i hate writing docs i hate writing docs i hate writing docs i hate writing docs

--- command/src/main/java/com/jagrosh/jdautilities/command/MessageContextMenuEvent.java
+++ command/src/main/java/com/jagrosh/jdautilities/command/MessageContextMenuEvent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016-2018 John Grosh (jagrosh) & Kaidan Gustave (TheMonitorLizard)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jagrosh.jdautilities.command;
 
 import net.dv8tion.jda.api.JDA;
@@ -13,6 +28,43 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
+/**
+ * <h1><b>Message Context Menus In JDA-Chewtils</b></h1>
+ *
+ * <p>The internal inheritance for Message Context Menus used in JDA-Chewtils is that of the object.
+ *
+ * <p>Classes created inheriting this class gain the unique traits of commands operated using the menu Extension.
+ * <br>Using several fields, a command can define properties that make it unique and complex while maintaining
+ * a low level of development.
+ * <br>All classes extending this class can define any number of these fields in a object constructor and then
+ * create the menu action/response in the abstract {@link MessageContextMenu#execute(MessageContextMenuEvent)} body:
+ *
+ * <pre><code> public class ExampleCmd extends MessageContextMenu {
+ *
+ *      public ExampleCmd() {
+ *          this.name = "Example";
+ *      }
+ *
+ *      {@literal @Override}
+ *      protected void execute(MessageContextMenuEvent event) {
+ *          event.reply("Hey look! This would be the bot's reply if this was a command!");
+ *      }
+ *
+ * }</code></pre>
+ *
+ * Execution is with the provision of a MessageContextInteractionEvent-CommandClient wrapper called a
+ * {@link MessageContextMenu} and is performed in two steps:
+ * <ul>
+ *     <li>{@link MessageContextMenu#run(MessageContextMenuEvent) run} - The menu runs
+ *     through a series of conditionals, automatically terminating the command instance if one is not met,
+ *     and possibly providing an error response.</li>
+ *
+ *     <li>{@link MessageContextMenu#execute(MessageContextMenuEvent) execute} - The menu,
+ *     now being cleared to run, executes and performs whatever lies in the abstract body method.</li>
+ * </ul>
+ *
+ * @author Olivia (Chew)
+ */
 public class MessageContextMenuEvent extends MessageContextInteractionEvent
 {
     private final CommandClient client;
--- command/src/main/java/com/jagrosh/jdautilities/command/UserContextMenu.java
+++ command/src/main/java/com/jagrosh/jdautilities/command/UserContextMenu.java
@@ -1,11 +1,62 @@
+/*
+ * Copyright 2016-2018 John Grosh (jagrosh) & Kaidan Gustave (TheMonitorLizard)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jagrosh.jdautilities.command;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.AudioChannel;
-import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
 
+/**
+ * <h1><b>User Context Menus In JDA-Chewtils</b></h1>
+ *
+ * <p>The internal inheritance for User Context Menus used in JDA-Chewtils is that of the object.
+ *
+ * <p>Classes created inheriting this class gain the unique traits of commands operated using the menu Extension.
+ * <br>Using several fields, a menu can define properties that make it unique and complex while maintaining
+ * a low level of development.
+ * <br>All classes extending this class can define any number of these fields in a object constructor and then
+ * create the menu action/response in the abstract {@link UserContextMenu#execute(UserContextMenuEvent)} body:
+ *
+ * <pre><code> public class ExampleCmd extends UserContextMenu {
+ *
+ *      public ExampleCmd() {
+ *          this.name = "Example";
+ *      }
+ *
+ *      {@literal @Override}
+ *      protected void execute(UserContextMenu event) {
+ *          event.reply("Hey look! This would be the bot's reply if this was a command!");
+ *      }
+ *
+ * }</code></pre>
+ *
+ * Execution is with the provision of a UserContextInteractionEvent-CommandClient wrapper called a
+ * {@link UserContextMenuEvent} and is performed in two steps:
+ * <ul>
+ *     <li>{@link UserContextMenu#run(UserContextMenuEvent) run} - The menu runs
+ *     through a series of conditionals, automatically terminating the command instance if one is not met,
+ *     and possibly providing an error response.</li>
+ *
+ *     <li>{@link UserContextMenu#execute(UserContextMenuEvent) execute} - The menu,
+ *     now being cleared to run, executes and performs whatever lies in the abstract body method.</li>
+ * </ul>
+ *
+ * @author Olivia (Chew)
+ */
 public abstract class UserContextMenu extends ContextMenu
 {
     /**
